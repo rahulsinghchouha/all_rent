d675f541b72b839811891e851be227757c0b7d4a
rahulsinghchouha <singhrahul15737@gmail.com>
2026-03-12 23:59:01 +0530

    rahul dev branch created

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,3 @@
-import Sidebar from "@/app/components/layout/sidebar";
 
 export default function DashboardLayout({
     children,
@@ -7,7 +6,6 @@ export default function DashboardLayout({
 }) {
     return (
         <div className="flex min-h-screen">
-            <Sidebar />
             <main className="flex-1 overflow-auto bg-[#f5f5f5] p-4">
                 {children}
             </main>
